b4091d4e3c619252020376c55819f7b4cf37111d
DavidR <david.readman@gmail.com>
2019-11-12 12:42:44 +1030

    explanatory documentation for main class

--- src/Harmony.java
+++ src/Harmony.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.util.LinkedList;
+
 import gov.nasa.worldwind.BasicModel;
 import gov.nasa.worldwind.Configuration;
 import gov.nasa.worldwind.Model;
@@ -51,29 +52,60 @@ import javax.swing.border.LineBorder;
 
 import javax.swing.*;
 
-public class Harmony {
-    public static void main (String[] args) throws IOException
+/*
+This project aims to provide a structure for the creation of multiple scenarios,
+ utilising external software to analyse current 'node' locations and return a set of metrics
+ that will allow for
+ a) a score based on these metrics or internal analysis
+ b) options for the next location of each node
+ c) a record of the node positions, the metrics returned and the decision made for the next location
+
+ The records made should provide the basis for an analysis for machine learning which can then be
+ used back into the program to refine the decision making process.
+
+ The primary interfaces in this project are:
+ a)initialisation file
+ 	- this file will contain the initial starting locations of the nodes and later extend
+ 	to providing the basic plan for the nodes.
+ 	- initial design is to use the java .properties file
+ b)data to external analysis software
+ 	- this will be implemented as DDS as this is the middleware being used for the Generic Vehicle
+ 	Architecture (GVA)
+ 	- target analysis software (SMARTNet) has an existing DDS implementation
+ c)data from external analysis software
+ 	- Decisions made as per b
+ d)recorded data
+ 	- this will be recorded as a comma separated variable (CSV) file to allow for simple import
+ 	and analysis by as many programs as possible.
+ 	- CSV chosen over XML due to time sequenced events
+ */
+public class Harmony
+{
+    public static void main(String[] args) throws IOException
     {
 
-    	//Create initialisations
-    	ParseProperties properties = new ParseProperties();
-		//set up data publisher (could also be done within the subclass guiactions)
-		HarmonyDataPublisher publishdata = new HarmonyDataPublisher();
-    	
-    	//Read in initialisation file
-    	NodeData[] blueNode = properties.parseConfig();
+        //Create initialisations
+		/*
+		ParseProperties class reads in values from .properties file for nodal information
+		 */
+        ParseProperties properties = new ParseProperties();
+        //set up data publisher (could also be done within the subclass guiactions)
+        HarmonyDataPublisher publishdata = new HarmonyDataPublisher();
 
-		//set up worldwind canvas
-		DisplayWW outputToDisplay = new DisplayWW(blueNode);
-    	//set up gui
-		new JFrameGuiActions(outputToDisplay,publishdata,blueNode);
+        //Read in initialisation file and load into the NodeData array
+        NodeData[] blueNode = properties.parseConfig();
+
+        //set up worldwind canvas
+        DisplayWW outputToDisplay = new DisplayWW(blueNode);
+        //set up gui and all actions thereafter
+        new JFrameGuiActions(outputToDisplay, publishdata, blueNode);
 
         // Make Decisions
-		//putting this within the guiactions initially
+        //putting this within the guiactions initially
         //Update WorldWind
-		//putting this within the guiactions
+        //putting this within the guiactions
         //Record this time epoch
-		//putting this within the guiactions
+        //putting this within the guiactions
 
     }
 
